d0326ea6a127fc069bc3dc3dc698f53ab204e8ba
Joan Sanchis <jsalvadorsb@gmail.com>
2025-04-02 13:23:51 +0200

    Tipado de DogCard y renderizado dinámico con useState y map

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,44 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+interface Dog {
+  imgURL: string;
+  like: number;
+  dislike: number;
+}
 
 function App() {
+  const [dogList, setDogList] = useState<Dog[]>([
+    {
+      imgURL:
+        "https://images.unsplash.com/photo-1517849845537-4d257902454a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBlcnJvfGVufDB8fDB8fHww",
+      like: 0,
+      dislike: 0,
+    },
+  ]);
 
   return (
     <>
-      <div className='dog-list'>
-        <div className='dog-card'>
-          <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBlcnJvfGVufDB8fDB8fHww" alt="" />
-          <div className='dog_votes'>
-            <span className='count-like'>0❤️</span>
-            <span className='count-dislike'>0🤮</span>
-          </div>
-          <div className='dog_buttons'>
-            <button className='button-like'>Like perrico</button>
-            <button className='button-dislike'>Dislike perrico</button>
-          </div>
-        </div>
-        <div className='dog-card'>
-          <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBlcnJvfGVufDB8fDB8fHww" alt="" />
-          <div className='dog_votes'>
-            <span className='count-like'>0❤️</span>
-            <span className='count-dislike'>0🤮</span>
-          </div>
-          <div className='dog_buttons'>
-            <button className='button-like'>Like perrico</button>
-            <button className='button-dislike'>Dislike perrico</button>
-          </div>
-        </div>
-        <div className='dog-card'>
-          <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBlcnJvfGVufDB8fDB8fHww" alt="" />
-          <div className='dog_votes'>
-            <span className='count-like'>0❤️</span>
-            <span className='count-dislike'>0🤮</span>
-          </div>
-          <div className='dog_buttons'>
-            <button className='button-like'>Like perrico</button>
-            <button className='button-dislike'>Dislike perrico</button>
-          </div>
-        </div>
+      <button>Add perrico</button>
+      <div className="dog-list">
+        {dogList.map((dog) => {
+          return (
+            <div className="dog-card">
+              <img src={dog.imgURL} alt="" />
+              <div className="dog_votes">
+                <span className="count-like">{dog.like}❤️</span>
+                <span className="count-dislike">{dog.dislike}🤮</span>
+              </div>
+              <div className="dog_buttons">
+                <button className="button-like">Like perrico</button>
+                <button className="button-dislike">Dislike perrico</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
